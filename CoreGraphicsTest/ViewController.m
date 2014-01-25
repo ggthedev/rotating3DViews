3e2593a1a6b3cd9a067f10e84857249775896e3e
Josh Grant <josgrant@deloitte.com>
2014-01-25 15:39:01 -0700

    Got the first two views working

--- CoreGraphicsTest/ViewController.m
+++ CoreGraphicsTest/ViewController.m
@@ -10,20 +10,98 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIPanGestureRecognizer *panGestureRecognizer;
+@property (nonatomic, strong) UIView *firstView;
+@property (nonatomic, strong) UIView *secondView;
+@property (nonatomic, strong) UIView *thirdView;
+
 @end
 
 @implementation ViewController
 
+#pragma mark - View lifecycle
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self.view addGestureRecognizer:self.panGestureRecognizer];
+    [self.view addSubview:self.firstView];
+    [self.view addSubview:self.secondView];
+    
+    [self panGestureRecognizerDidPan:self.panGestureRecognizer];
 }
 
-- (void)didReceiveMemoryWarning
+#pragma mark - Gesture recognizer
+
+- (void)panGestureRecognizerDidPan:(UIPanGestureRecognizer *)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSLog(@"%f", [sender locationInView:self.view].x);
+
+    CGFloat percentage = [sender locationInView:self.view].x / 320;
+    
+    [self animateFirstViewWithPercentage:percentage];
+    [self animateSecondViewWithPercentage:percentage];
+}
+
+#pragma mark - Animations
+
+- (void)animateFirstViewWithPercentage:(CGFloat)percentage
+{
+    CATransform3D transform = CATransform3DIdentity;
+    transform.m34 = 1.0 / -500;
+    
+    transform = CATransform3DRotate(transform, M_PI_2 * percentage, 0, 1, 0);
+    transform = CATransform3DTranslate(transform, 0 + 160 * percentage, 0, 0 + 160 * percentage);
+    
+    self.firstView.layer.transform = transform;
+}
+
+- (void)animateSecondViewWithPercentage:(CGFloat)percentage
+{
+    CATransform3D transform = CATransform3DIdentity;
+    transform.m34 = 1.0 / -500;
+    
+    transform = CATransform3DRotate(transform, M_PI_2 * percentage - M_PI_2, 0, 1, 0);
+    transform = CATransform3DTranslate(transform, -160 + 160 * percentage, 0, 160 - 160 * percentage);
+    
+    self.secondView.layer.transform = transform;
+}
+
+#pragma mark - Overridden properties
+
+- (UIPanGestureRecognizer *)panGestureRecognizer
+{
+    if (_panGestureRecognizer == nil) {
+        _panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGestureRecognizerDidPan:)];
+    }
+    return _panGestureRecognizer;
+}
+
+- (UIView *)firstView
+{
+    if (_firstView == nil) {
+        _firstView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 568)];
+        [_firstView setBackgroundColor:[UIColor redColor]];
+    }
+    return _firstView;
+}
+
+- (UIView *)secondView
+{
+    if (_secondView == nil) {
+        _secondView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 568)];
+        [_secondView setBackgroundColor:[UIColor greenColor]];
+    }
+    return _secondView;
+}
+
+- (UIView *)thirdView
+{
+    if (_thirdView == nil) {
+        _thirdView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 568)];
+        [_thirdView setBackgroundColor:[UIColor blueColor]];
+    }
+    return _thirdView;
 }
 
 @end
